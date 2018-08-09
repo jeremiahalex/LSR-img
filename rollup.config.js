f5cf4406f1d89d09a8eacae26b877d12305e4614
Carlos E. Alfaro M <carlos.morales@playkids.com>
2018-08-09 15:02:06 -0300

    Updated to use like a module. Zip library changed to a more flexible one, and eliminate the dependency of JQuery.

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import json from 'rollup-plugin-json';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import images from 'rollup-plugin-image';
+//import eslint from 'rollup-plugin-eslint';
+
+import { main, module } from './package.json';
+const env = process.env.NODE_ENV;
+
+export default {
+  input: 'src/lsr-img.js',
+  output: {
+    file: {
+      es: module,
+      umd: main,
+    }[env],
+    format: env,
+    name: 'LSRImg',
+    globals: {
+      jszip: 'JSZip'
+    }
+  },
+  plugins: [
+    json(),
+    resolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**',
+      externalHelpers: true
+    }),
+    images(),
+    //eslint()
+  ],
+  external: ['jszip']
+};
